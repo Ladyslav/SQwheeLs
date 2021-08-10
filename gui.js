d3472aabdf32c366a3586703f5b316afb5540cec
ShemetViktoriia <shemetvic@ukr.net>
2021-08-10 19:36:47 +0300

    fix something

--- gui.js
+++ gui.js
@@ -57,6 +57,7 @@ var tableCreate = function () {
 	}
 	return function (columns, values) {
 		var tbl = document.createElement('table');
+		tbl.classList.add('table');
 		var html = '<thead>' + valconcat(columns, 'th') + '</thead>';
 		var rows = values.map(function (v) { return valconcat(v, 'td'); });
 		html += '<tbody>' + valconcat(rows, 'tr') + '</tbody>';
